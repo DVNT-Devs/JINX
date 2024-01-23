948836d9d8c3f10dde3c64eb777b9e6c16ee590b
PineaFan <ash@pinea.dev>
2024-01-23 20:17:12 +0000

    Update degrade.ts

--- src/commands/degrade.ts
+++ src/commands/degrade.ts
@@ -7,6 +7,7 @@ import { responseFrom } from "../actions/randomResponses";
 const insult = new SlashCommandBuilder()
     .setName("degrade")
     .setDescription("Degrade a fellow member, or yourself")
+    .addUserOption(option => option.setName("user").setDescription("The user to insult | Default: Yourself").setRequired(false))
     .addStringOption(option => option
         .setName("type")
         .setDescription("The type of insult to use | Default: ")
@@ -14,8 +15,7 @@ const insult = new SlashCommandBuilder()
             { name: "Degradation", value: "degrade" },
             { name: "Mean", value: "mean" }
         ).setRequired(false)
-    )
-    .addUserOption(option => option.setName("user").setDescription("The user to insult | Default: Yourself").setRequired(false));
+    );
 
 
 const callback = async (interaction: CommandInteraction) => {
