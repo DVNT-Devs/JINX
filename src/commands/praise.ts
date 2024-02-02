9138491fbd5c0e4945e68fe2d50f11330b2ce4d4
pineafan <ash@pinea.dev>
2024-02-02 21:48:22 +0000

    Added unverified kick feature and updated praise/degrade

--- src/commands/praise.ts
+++ src/commands/praise.ts
@@ -1,7 +1,9 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { CommandInteraction, GuildMember } from "discord.js";
+import data from "../data";
 
 import { responseFrom } from "../actions/randomResponses";
+import { backfireResponse } from "../actions/backfire";
 
 
 const praise = new SlashCommandBuilder()
@@ -12,13 +14,21 @@ const praise = new SlashCommandBuilder()
 
 const callback = async (interaction: CommandInteraction) => {
     const user = interaction.options.getUser("user") || interaction.user;
-    const target = interaction.guild!.members.cache.get(user.id) as GuildMember;
+    let target = interaction.guild!.members.cache.get(user.id) as GuildMember;
+
+    let message = "";
+    // Check if the target is exempt from being praised
+    if (data.exemptions.praise.includes(target.id)) {
+        message = backfireResponse(target.id, "praise");
+        target = interaction.guild!.members.cache.get(interaction.user.id) as GuildMember;
+    }
+
     let id = target.id;
     if (target.id === interaction.guild?.members.me!.id) id = interaction.user.id;
 
     const response = responseFrom(target as GuildMember, "praise");
 
-    void interaction.reply(`<@${id}>\n\n${response}`);
+    void interaction.reply(`${message}<@${id}>\n\n${response}`);
 };
 
 
--- src/context/user/praise.ts
+++ src/context/user/praise.ts
@@ -1,5 +1,7 @@
 import { ContextMenuCommandBuilder, GuildMember, UserContextMenuCommandInteraction } from "discord.js";
 import { responseFrom } from "../../actions/randomResponses";
+import data from "../../data";
+import { backfireResponse } from "../../actions/backfire";
 
 const command = new ContextMenuCommandBuilder()
     .setName("Praise")
@@ -7,13 +9,21 @@ const command = new ContextMenuCommandBuilder()
 
 const callback = async (interaction: UserContextMenuCommandInteraction) => {
     const user = interaction.options.getUser("user") || interaction.user;
-    const target = interaction.guild!.members.cache.get(user.id) as GuildMember;
+    let target = interaction.guild!.members.cache.get(user.id) as GuildMember;
+
+    let message = "";
+    // Check if the target is exempt from being praised
+    if (data.exemptions.praise.includes(target.id)) {
+        message = backfireResponse(target.id, "praise");
+        target = interaction.guild!.members.cache.get(interaction.user.id) as GuildMember;
+    }
+
     let id = target.id;
     if (target.id === interaction.guild?.members.me!.id) id = interaction.user.id;
 
     const response = responseFrom(target as GuildMember, "praise");
 
-    await interaction.reply(`<@${id}>\n\n${response}`);
+    await interaction.reply(`${message}<@${id}>\n\n${response}`);
 };
 
 export { command, callback };
