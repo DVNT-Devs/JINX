fa4d848faeea43c7f2c14f27cc37f595df1c6112
PineaFan <ash@pinea.dev>
2024-02-14 18:21:56 +0000

    Changed database loading system (#28)

--- src/database/drizzle.ts
+++ src/database/drizzle.ts
@@ -1,13 +1,24 @@
+import { readFileSync } from "fs";
 import { drizzle, type PostgresJsDatabase } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-const migrationClient = postgres(process.env["DATABASE_URL"] || "", { max: 1, database: "jinx" });
+const env = process.env;
+let databaseUrl = "";
+if (!env["DATABASE_USER"]) {
+    databaseUrl = `postgresql://${env["DATABASE_HOST"]}`;
+} else {
+    const password = readFileSync(env["DATABASE_PASSWORD_FILE"]!, "utf-8");
+    databaseUrl = `postgresql://${env["DATABASE_USER"]}:${password}@${env["DATABASE_HOST"]}`;
+}
+
+console.log(databaseUrl);
+const migrationClient = postgres(databaseUrl, { max: 1, database: "jinx" });
 
 const DB: Promise<PostgresJsDatabase<typeof schema>> = (async () => {
     await migrate(drizzle(migrationClient, { schema }), { migrationsFolder: "./src/database/migration" });
-    const queryClient = postgres(process.env["DATABASE_URL"] || "", { database: "jinx" });
+    const queryClient = postgres(databaseUrl, { database: "jinx" });
     return drizzle(queryClient, { schema });
 })();
 
