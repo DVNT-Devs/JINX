0e316a63fcefa75b81f2c4dae581f1a579f4d9ce
pineafan <ash@pinea.dev>
2024-03-02 13:20:14 +0000

    Unverified kicking is locked and loaded

--- src/actions/kickUnverified.ts
+++ src/actions/kickUnverified.ts
@@ -1,8 +1,8 @@
-import { GuildMember } from "discord.js";
+import { GuildMember, GuildTextBasedChannel } from "discord.js";
 import { JinxClient } from "../client";
 
 
-const secondsPerKick = 1.1;
+const secondsPerKick = 1.25;
 const kicksPerSecond = 1 / secondsPerKick;
 
 export default async (client: JinxClient) => {
@@ -10,7 +10,35 @@ export default async (client: JinxClient) => {
     client.purgeLock = true;
     const guild = client.guilds.cache.get(process.env["HOST_GUILD"] || "")!;
     const verifiedRole = "1164256198489538653";
+    const sendChannelId = "1213474410996957285";
+
+    const emojiIDs = [
+        "1169338343851557045",  "1169815358824263720",  "1179834075234713611",
+        "a1169031440541945977", "a1179590287606169711", "1169475073107832954",
+        "a1179834077336064050", "1169031443855458435",  "1169031822626279595"
+    ];
+    const phrases = [
+        "You will not be missed",
+        "Goodbye",
+        "Don't let the door hit you on the way out",
+        "Send your mom my regards",
+        "Send a postcard",
+        "Good riddance",
+        "Nice knowing you",
+        "Sayonara",
+        "You now have my permission to cry",
+        "Hasta la vista",
+        "Viva la revolución"
+    ];
+
+    const emojiIDToMention = (id: string) => {
+        return id.startsWith("a") ? `<a:N:${id.slice(1)}>` : `<:N:${id}>`;
+    };
+    const selectRandomEmoji = () => { return emojiIDToMention(emojiIDs[Math.floor(Math.random() * emojiIDs.length)]!); };
+    const selectRandomPhrase = () => { return phrases[Math.floor(Math.random() * phrases.length)]; };
+
     const members = await guild.members.fetch();
+    const sendChannel = guild.channels.cache.get(sendChannelId)! as GuildTextBasedChannel;
 
     const unverifiedMembers = Array.from(members
         .filter(member => !member.roles.cache.has(verifiedRole))
@@ -31,9 +59,15 @@ export default async (client: JinxClient) => {
         } else {
             await fetchedMember.kick();
         }
+        const joinTimestamp = Math.floor(fetchedMember.joinedAt!.getTime() / 1000);
+        await sendChannel.send(
+            `${selectRandomEmoji()} Kicked ${fetchedMember.user.username} (Joined <t:${joinTimestamp}:R>) ` +
+            `- ${selectRandomPhrase()}`
+        );
     };
 
-    console.log(`${new Date().toLocaleTimeString()} - Kicking ${unverifiedMembers.length} unverified members...`);
+    console.log(`\x1b[31m${new Date().toLocaleTimeString()} - Kicking ${unverifiedMembers.length} unverified members...`);
+    console.log(`ETA: ${Math.round((unverifiedMembers.length * secondsPerKick) / 60)} minutes\x1b0`);
     await setInterval(() => {
         if (i < unverifiedMembers.length) {
             void execute(i);
